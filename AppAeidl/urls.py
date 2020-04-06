1c11361074d11699691443344fb8b9a418869b75
Jaime Hernandez <jh.zina@nokia.com>
2020-04-05 20:49:19 -0500

    [AEIDL-1] Add templates and funcionality to login and see dashboard

--- AppAeidl/urls.py
+++ AppAeidl/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 # from rest_framework.routers import DefaultRouter
 # from AppAeidl.views import PatientView, MedicView, AnalisisView, download_file, EntityView, AppointmentView, StudyView
@@ -11,9 +13,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # router.register(r'appointment', AppointmentView)
 # router.register(r'study', StudyView)
 #
+from Aeidl import settings
+from AppAeidl.views import Home
+
 urlpatterns = [
-    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token_refresh', TokenRefreshView.as_view(), name='token_refresh'),
     # path('', include(router.urls)),
     # path('download_file', download_file, name='download_file')
-]
+    path('', Home.as_view(), name="home"),
+    path('login/', LoginView.as_view(template_name='Aeidl/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='Aeidl/login.html'), name='logout')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
